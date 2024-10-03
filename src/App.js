d962b2d3e2492e2776088f6f5a0745798e507246
Capitanzz <122799856+Kyrylo234@users.noreply.github.com>
2024-10-03 23:40:46 +0100

    Made the app responsive
    Now if the width of the device decreases enough it will switch to the phone version of the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import DesktopApp from './DesktopApp.jsx';
 import Phone from './Phone.jsx';
 function App() {
-  const isMobile = window.innerWidth < 768;
+  const [content,changeContent] = useState(<DesktopApp/>);
+
+  const handleResize = () => {
+    if(window.innerWidth < 768){
+      if(content!==<Phone/>){
+        changeContent(<Phone/>)
+      }
+    }else{
+      if(content!==<DesktopApp/>){
+        changeContent(<DesktopApp/>)
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Add the event listener when the component mounts
+    window.addEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <>
-    {isMobile ? <Phone/> : <DesktopApp/>}
-    </>
+    <div>
+    {content}
+    </div>
   );
 }
 
