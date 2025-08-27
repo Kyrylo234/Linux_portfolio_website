a713a1974865d379f1c68d1ba94a12121516dcd5
Kyrylo234 <kyrylop2004@gmail.com>
2025-08-27 12:49:29 +0100

    added new images to projects

--- src/ProjectsApplication.jsx
+++ src/ProjectsApplication.jsx
@@ -1,5 +1,8 @@
 import Project from "./Project"
-import Website from"./assets/Website.png"
+import Website from"./assets/website1.png"
+import Website2 from"./assets/website2.png"
+import Website3 from"./assets/website3.png"
+import Website4 from"./assets/website4.png"
 import CSPPT from "./assets/CSPPT.png"
 import Database from "./assets/Database.jpeg"
 import carApp1 from "./assets/carApp1.png"
@@ -12,7 +15,7 @@ function ProjectsApplication() {
         <>  
             <div className="PhoneApplication" style={{paddingTop:"20px", paddingBottom:"60px"}}>
               <h2>Projects</h2>
-              <Project source={[Website]} elements={["JavaScript", "React.js", "CSS", "Git"]} title={"Portfolio Website"} imgAlt={"Website"}/>
+              <Project source={[Website, Website2, Website3, Website4]} elements={["JavaScript", "React.js", "CSS", "Git"]} title={"Portfolio Website"} imgAlt={"Website"}/>
               <Project source={[carApp1, carApp2, carApp3, carApp4]} elements={["Dart", "Flutter (Dart)"]} title={"Car Service Tracking App"}/>
               <Project source={[CSPPT]} elements={["Java", "Swing (Java)", "Teamwork", "Git"]} title={"CSPPT"}/>
               <Project source={[Database]} elements={["PHP", "HTML/CSS", "SQL"]} title={"Database Of Books Read"}/>
