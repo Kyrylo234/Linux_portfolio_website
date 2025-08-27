88cea7bd4b594d5488584631034ef4ba194d063a
Kyrylo234 <kyrylop2004@gmail.com>
2025-08-27 11:37:20 +0100

    changed projects page to use the RandomColorDiv instead

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,17 +1,11 @@
+import RandomColorDiv from "./RandomColorDiv";
 function Project({source, elements}) {  
-  function generateRandColour(){
-    let value = Math.floor(Math.random()*16777215).toString(16);
-    if(value.length<=5){
-      value+='0'
-    }
-    return value;
-  }
     return (
       <>
       <div style={{marginTop:'20px'}}>
         <img src={source} style={{width:'90vw', height:'auto'}} alt=""></img>
         {elements.map((text, i) => (
-        <div key={i} style={{backgroundColor:'#'+generateRandColour()}} className="ColouredDiv"><p>{text}</p></div>
+        <RandomColorDiv key={i} text={text}/>
       ))}
       </div>
       </>
