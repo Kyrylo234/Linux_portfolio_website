e3aae9944645fd7b751e7498a1ade4fdcd275467
Kyrylo234 <kyrylop2004@gmail.com>
2025-09-03 22:47:34 +0100

    added a button to download cv in cv section

--- src/CV.jsx
+++ src/CV.jsx
@@ -37,6 +37,17 @@ function RunningApplication({ appWindow, setWindows, onClose, onToggleVisibility
               msOverflowStyle: "none" // IE/Edge
             }}
           >
+            <a href="/assets/Kyrylo_Postupalenko_CV.pdf" download="cv" style={{ textDecoration: "none" }}>
+              <button
+                style={{
+                  padding: "8px 14px",
+                  borderRadius: "6px",
+                  border: "none",
+                  backgroundColor: "#444",
+                  color: "white",
+                  cursor: "pointer"
+              }}>Download my CV</button>
+            </a>
             {<PDFViewer file="/assets/Kyrylo_Postupalenko_CV.pdf" />}
           </div>
         </div>
--- src/CVApplication.jsx
+++ src/CVApplication.jsx
@@ -2,6 +2,17 @@ import PDFViewer from "./PDFViewer"
 function CVApplication() {  
       return (
         <div className="PhoneApplication" style={{paddingTop:"20px", paddingBottom:"60px"}}>
+          <a href="/assets/Kyrylo_Postupalenko_CV.pdf" download="cv" style={{ textDecoration: "none" }}>
+            <button
+              style={{
+                padding: "8px 14px",
+                borderRadius: "6px",
+                border: "none",
+                backgroundColor: "#444",
+                color: "white",
+                cursor: "pointer"
+            }}>Download my CV</button>
+          </a>
           <PDFViewer file="/assets/Kyrylo_Postupalenko_CV.pdf" />
         </div>
       )
