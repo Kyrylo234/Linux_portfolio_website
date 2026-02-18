5186f4868cf8ebc9da43392db25c3d39811944dc
Kyrylo234 <kyrylop2004@gmail.com>
2026-02-18 18:16:16 +0000

    CSPPT language bars were wrong

--- src/components/shared/Projects/CSPPTProject.jsx
+++ src/components/shared/Projects/CSPPTProject.jsx
@@ -11,11 +11,8 @@ import Photo5 from "../../../assets/images/CSPPT/image5.png"
 
 function CSPPTProject() {
     return (
-      <Project source={[Photo1, Photo2, Photo3, Photo4, Photo5]} elements={[<img src="https://img.shields.io/badge/-Python-3776AB?style=flat&logo=python&logoColor=white" style={{ height: "20px", margin: "0px", width: "auto" }}/>
-      ,<img src="https://img.shields.io/badge/-Pillow-FFD43B?style=flat&logo=pillow&logoColor=white" style={{ height: "20px", margin: "0px", width: "auto" }}/>
-      ,<img src="https://img.shields.io/badge/-NumPy-013243?style=flat&logo=numpy&logoColor=white" style={{ height: "20px", margin: "0px", width: "auto" }}/>
-      ,<img src="https://img.shields.io/badge/-Trimesh-FF6600?style=flat&logoColor=white" style={{ height: "20px", margin: "0px", width: "auto" }}/>
-      ,<img src="https://img.shields.io/badge/-Gradio-1DA1F2?style=flat&logo=gradio&logoColor=white" style={{ height: "20px", margin: "0px", width: "auto" }}/>]} 
+      <Project source={[Photo1, Photo2, Photo3, Photo4, Photo5]} elements={[<img src="https://img.shields.io/badge/-Java-007396?style=flat&logo=java&logoColor=white" style={{ height: "20px", margin: "0px", width: "auto" }}/>
+      ,<img src="https://img.shields.io/badge/-Git-F05032?style=flat&logo=git&logoColor=white" style={{ height: "20px", margin: "0px", width: "auto" }}/>]} 
       title={"Computer Science PowerPoint"} link={"https://github.com/Kyrylo234/CSPPT"} desc={"A presentation tool for computer science students and developers, designed to go beyond traditional slides by integrating UML diagrams, a built-in command line, and live coding support — all inside your slides."}/>
     )
   }
